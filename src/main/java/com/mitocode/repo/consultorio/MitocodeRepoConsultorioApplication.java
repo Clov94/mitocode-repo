4fad315a4bc7a0494a7b7406861344df0cdff817
lvillanueva-clgx <lvillanueva@corelogic.com>
2024-04-08 13:09:55 -0600

    consultorio setup

---
+++ src/main/java/com/mitocode/repo/consultorio/MitocodeRepoConsultorioApplication.java
@@ -0,0 +1,13 @@
+package com.mitocode.repo.consultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitocodeRepoConsultorioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MitocodeRepoConsultorioApplication.class, args);
+    }
+
+}
