71512e9db810e4eac9c0ee1ff9801725d95cdecd
lvillanueva-clgx <lvillanueva@corelogic.com>
2024-04-09 13:19:54 -0600

    master refactor

--- src/main/java/com/mitocode/repo/MitocodeRepoApplication.java
+++ src/main/java/com/mitocode/repo/MitocodeRepoApplication.java
@@ -1,13 +1,13 @@
-package com.mitocode.repo.consultorio;
+package com.mitocode.repo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MitocodeRepoConsultorioApplication {
+public class MitocodeRepoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MitocodeRepoConsultorioApplication.class, args);
+        SpringApplication.run(MitocodeRepoApplication.class, args);
     }
 
 }
